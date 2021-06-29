36809ae1c3c76fbbe976d365438988d06d1f7f5b
mbrown <tugtuj@gmail.com>
2021-06-29 11:46:00 -0400

    Set-up

--- bangerz/src/index.js
+++ bangerz/src/index.js
@@ -1,5 +1,12 @@
  const express = require('express')
+ const axios = require('axios')
+
 
  const app = express()
 
+ app.get('/', (req, res) => {
+     res.send('Spotify API')
+ })
+
+ // https://developer.spotify.com/console/get-recommendations/
  app.listen(3000, console.log(`Serving is running on port 3000`))
